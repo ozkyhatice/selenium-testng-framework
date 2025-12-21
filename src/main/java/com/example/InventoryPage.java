e386581336482cd9d0784f0f4e7e38a3c62fdd08
Hatice Özkaya <ozkyyhatice@gmail.com>
2025-12-21 16:55:23 +0300

    Implement InventoryPage and InventoryTest classes with Selenium actions and test cases

---
+++ src/main/java/com/example/InventoryPage.java
@@ -0,0 +1,38 @@
+package com.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class InventoryPage {
+    private WebDriver driver;
+
+    //LOCATORS
+    private By backPackItem = By.id("item_4_img_link");
+    private By priceItem = By.className("inventory_details_price");
+    private By addToCartButton = By.id("add-to-cart");
+    private By shoppingCartBadge = By.className("shopping_cart_badge");
+
+    //CONSTRUCTOR
+    public InventoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    //ACTIONS
+    public void clickBackpack() {
+        driver.findElement(backPackItem)
+            .click();
+    }
+    public String getBackpackPrice() {
+        return driver.findElement(priceItem)
+            .getText();
+    }
+    public void clickAddToCart() {
+        driver.findElement(addToCartButton)
+            .click();
+    }
+    public String getCartBadgeCount(WebDriverWait wait) {
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(shoppingCartBadge));
+        return driver.findElement(shoppingCartBadge)
+            .getText();
+    }
+}
---
+++ src/test/java/com/example/InventoryTest.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class InventoryTest {
+    WebDriver driver;
+    WebDriverWait wait;
+    LoginPagePom loginPage;
+    InventoryPage inventoryPage;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://www.saucedemo.com/");
+        loginPage = new LoginPagePom(driver);
+        inventoryPage = new InventoryPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void addBackpackToCart() {
+        inventoryPage.clickBackpack();
+        String price = inventoryPage.getBackpackPrice();
+        Assert.assertEquals(price, "$29.99", "Backpack price does not match.");
+        inventoryPage.clickAddToCart();
+        String badgeCount = inventoryPage.getCartBadgeCount(wait);
+        Assert.assertEquals(badgeCount, "1", "Cart badge count should be 1 after adding an item.");
+    }
+}
